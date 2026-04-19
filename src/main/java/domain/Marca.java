fa5691c40b79384d4b53e196e35bca04d23aa121
nsa2412 <ale.nicolas.salomon@gmail.com>
2026-04-19 17:28:13 -0300

    se crea la clase marca

--- src/main/java/domain/Marca.java
+++ src/main/java/domain/Marca.java
@@ -2,14 +2,29 @@
 package domain;
 
 public class Marca {
-    private String descripcion;
+    private String nombre;
+    private String pais;
 
-    public Marca(String descripcion) {
-        this.descripcion = descripcion;
+    public Marca(String nombre, String pais) {
+        this.nombre = nombre;
+        this.pais = pais;
     }
 
-    public String getDescripcion() {
-        return descripcion;
+    
+    public String getNombre() {
+        return nombre;
     }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+    
 }
